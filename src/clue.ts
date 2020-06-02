676bb226c2f0a7298f095893000377ac644df76d
Phillip Manwaring <phillip.manwaring@gmail.com>
2020-06-02 13:49:38 -0400

    separate clue and create clue request objects

--- src/clue.ts
+++ src/clue.ts
@@ -1,20 +1,9 @@
+import { Team } from './team';
+
 /**
  *  @swagger
  *  components:
  *    schemas:
- *
- *      CreateClueRequest:
- *        type: object
- *        required:
- *          - word
- *          - tiles
- *        properties:
- *          word:
- *            description: The single word clue
- *            type: string
- *          tiles:
- *            description: How many tiles this clue is for
- *            type: number
  *
  *      Clue:
  *        type: object
@@ -33,4 +22,32 @@ export interface Clue {
   id: string;
   word: string;
   tiles: number;
+}
+
+/**
+ *  @swagger
+ *  components:
+ *    schemas:
+ *
+ *      CreateClueRequest:
+ *        type: object
+ *        required:
+ *          - word
+ *          - tiles
+ *          - team
+ *        properties:
+ *          word:
+ *            description: The single word clue
+ *            type: string
+ *          tiles:
+ *            description: How many tiles this clue is for
+ *            type: number
+ *          team:
+ *            description: Which team is giving this clue
+ *            $ref: '#/components/schemas/Team'
+ */
+export interface CreateClueRequest {
+  word: string;
+  tiles: number;
+  team: Team;
 }
